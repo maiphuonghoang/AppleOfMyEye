81ae53815e0738c190f4274d00d1895e28850552
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-10-26 17:07:47 +0700

    deque

--- CTDLGT/BuoiHocChinh/Buoi13/deque.cpp
+++ CTDLGT/BuoiHocChinh/Buoi13/deque.cpp
@@ -15,22 +15,69 @@ int basicDeque(){
 }
 //----------------------------------------------------------------
 /* Sliding window maximum
-deque: O(n), multiset: O(nlogn)
+lopp: O(nk), deque: O(n), multiset: O(nlogn)
+n = 9, k=3;
+7 3 1 2 8 6 4 1 0 
+=> 7 3 8 8 8 6 4
 */
-void normalSlidingWindow(){
+void loopSlidingWindow(){
     int n, k; cin >> n >> k;
     int a[n];
     for (int &x : a) cin >> x;
     //duyệt qua k cửa sổ 
     for (int i = 0; i <= n-k; i++){
         int tmp = INT_MIN;
-        
+        for (int j = 0; j < k; j++){
+            tmp = max(tmp, a[i+j]);
+        }
+        cout << tmp << " ";
     }
 }
+void multisetSlidingWindow(){
+    int n, k; cin >> n >> k;
+    int a[n];
+    for (int &x : a) cin >> x;
+    
+    multiset<int> ms;
+    for (int i = 0; i < k; i++){
+        ms.insert(a[i]);
+    }
+    cout << *ms.rbegin() << " ";//truy cập vào ptu cuối cùng trong multiset 
+
+    for (int i = k; i <= n-1; i++){
+        //cửa sổ mới bị xóa ptu ở vị trí i-k
+        //thêm ptu ở chỉ số i hiện tại vào 
+        ms.erase(ms.find(a[i-k]));//chỉ xóa iterator trỏ đến ptu
+        ms.insert(a[i]); 
+        cout << *ms.rbegin() << " ";
+    }
+}
+void dequeSlidingWindow(){
+    int n, k; cin >> n >> k;
+    int a[n];
+    for (int &x : a) cin >> x;
+    
+    deque<int> q;
+    for(int i = 0; i < k; i++){
+        while(!q.empty() && a[i] > a[q.back()])
+            q.pop_back();
+        q.push_back(i);
+    }
+    cout << a[q.front()] << " ";
+    for (int i = k; i < n; i++){
+        if(q.front() <= i - k)
+            q.pop_front();
+        while(!q.empty() && a[i] > a[q.back()])
+            q.pop_back();
+        q.push_back(i);
+        cout << a[q.front()] << " ";
+    }
+}
+
 int main() {
     #ifndef ONLINE_JUDGE 
     freopen ("D:\\AppleOfMyEye\\CTDLGT\\input.txt", "r", stdin);
     #endif
-    basicDeque();
+    dequeSlidingWindow();
     return 0;
 }
