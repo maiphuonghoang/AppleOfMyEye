6f297fb816a3a92e520fbd2138daa85540dab87d
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-11-04 00:11:29 +0700

    Thuật toán Kruskal tìm MTS

--- C++/BaiTap/DoThi/MinimunSpanningTree/Kruskal.cpp
+++ C++/BaiTap/DoThi/MinimunSpanningTree/Kruskal.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/**
+6 9
+1 2 12 
+1 3 4 
+2 3 1 
+2 4 5
+2 5 3
+3 5 2
+4 5 3
+4 6 10
+5 6 8
+=> MST: 18
+2 3 1
+3 5 2
+4 5 3
+1 3 4
+5 6 8
+*/
+
 const int maxn = 1001;
 int n, m;
 int parent[maxn], size[maxn];
@@ -31,10 +50,53 @@ bool Union(int a, int b){
     size[a] += size[b];
     return true;
 }
+void input(){
+    cin >> n >> m;
+    for (int i = 0; i < m; i++){
+        int x, y, w; cin >> x >> y >> w;
+        edge e;
+        e.u = x; e.v = y; e.w = w;
+        canh.push_back(e);//đẩy cạnh vào danh sách cạnh 
+    }
+}
+bool cmp(edge a, edge b){
+    //comparator theo chiều dài tăng dần
+    return a.w < b.w;
+}
+void kruskal(){
+    //B1: Tạo cây khung cực tiểu rỗng 
+    vector<edge> mst;
+    int d = 0;
+    //B2: Sort danh sách cạnh theo chiều dài tăng dần 
+    sort(canh.begin(), canh.end(), cmp);
+    //B3: Lặp
+    for (int i = 0; i < m; i++){//duyệt qua tất cả cạnh 
+        if(mst.size() == n-1) break;
+        edge e = canh[i]; //chọn cạnh e là cạnh nhỏ nhất 
+        if(Union(e.u, e.v))//có thể gộp 2 đỉnh của cạnh này vào mà nó k tạo thành chu trình 
+        {
+            mst.push_back(e); //đẩy cạnh e vào cây khung
+            d += e.w;
+        }
+    }
+    //Trả về kết quả 
+    if(mst.size() != n-1) cout << "Do thi khong lien thong. Khong ton tai cay khung" << endl;
+    else {
+        cout << "MST: " << d << endl;
+        for (auto it : mst){
+            cout << it.u << " " << it.v << " " << it.w << endl;
+        }
+    }
+}
+void run(){
+    input();
+    make_set();
+    kruskal();
+}
 int main() {
     #ifndef ONLINE_JUDGE 
     freopen ("D:\\AppleOfMyEye\\C++\\input.txt", "r", stdin);
     #endif
-
+    run();
     return 0;
 }
