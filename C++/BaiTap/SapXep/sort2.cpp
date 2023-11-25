6658d9322c713818dcd236b9d327edc08b21ab84
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-11-25 19:17:39 +0700

    bài tập sắp xếp và tìm kiếm 2

--- C++/BaiTap/SapXep/sort2.cpp
+++ C++/BaiTap/SapXep/sort2.cpp
@@ -133,7 +133,7 @@ int last_pos(int a[], int l, int r, int x){
     }
     return res;
 }
-void run(){//Tìm kiếm nhị phân 
+void run5(){//Tìm kiếm nhị phân 
     int n, k; cin >> n >> k;
     f0(i, n) cin >> a[i];
     sort(a, a+n);
@@ -145,6 +145,254 @@ void run(){//Tìm kiếm nhị phân
         if(l != -1) ans += r-l+1;
     }
     cout << ans << endl;
+}
+void run6(){//lower_bound, upper_bound
+    int n, k; cin >> n >> k;
+    f0(i, n) cin >> a[i];
+    sort(a, a+n);
+    ll ans = 0;
+    for(int i = 0; i<n-1; i++){
+        //tìm sau vị trí i 
+        auto l = lower_bound(a + i + 1, a+n, k - a[i]);//vị trí đầu tiên của ptu >= x
+        auto r = upper_bound(a + i + 1, a+n, k - a[i]);//vị trí đầu tiên của ptu > x
+        if(l != r)
+            ans += r-l;         
+    }
+    cout << ans << endl;
+}
+//----------------------------------------------------------------
+/**Cặp số có tổng nhỏ hơn k 
+ * a[i] + a[j] < k
+ * a[j] < k - a[i]
+ * Duyệt tất cả a[i] trong mảng đã sắp xếp, tìm xem có ptu < k-a[i]
+k = 6
+1 2 3 4 7   6-1=5
+  | | |
+
+2
+4 5
+2 2 2 2 
+3 4 
+1 2 4
+*/
+//Tìm vị trí xuất hiện cuối cùng của 1 ptu < x trong đoạn l-r
+int last_pos_smaller(int a[], int l, int r, int x){
+    int res = -1;
+    while(l <= r){
+        int m = (l+r)/2;
+        if(a[m] < x) {
+            res = m;
+            l = m + 1;//tìm thêm ở bên phải 
+        }
+        else r = m - 1;
+    }
+    return res;
+}
+void run7(){
+    int n, k; cin >> n >> k;
+    f0(i, n) cin >> a[i];
+    sort(a, a+n);
+    ll ans = 0;
+    for(int i = 0; i<n-1; i++){
+        //a[i]=>a[j]<k-a[i]
+        int l = last_pos_smaller(a, i+1, n-1, k-a[i]);
+        if(l != -1)
+            ans += l-i;
+    }
+    cout << ans << endl;
+}
+//----------------------------------------------------------------
+/**Cặp số có tổng lớn hơn k 
+ * a[i] + a[j] > k
+ * a[j] > k - a[i]
+ * 
+k = 4 |  1 4 6 8 9 
+Tìm ptu đầu tiên lớn hơn, những ptu sau chắc chắn lớn hơn k-a[i]
+
+2
+4 5
+2 3 4 5 
+3 3 
+1 2 3 
+*/
+int first_pos_greater(int a[], int l, int r, int x){
+    int res = -1;
+    while(l <= r){
+        int m = (l+r)/2;
+        if(a[m] > x) {
+            res = m;
+            r = m - 1;//tìm thêm ở bên trái  
+        }
+        else l = m + 1;
+    }
+    return res;
+}
+void run8(){
+    int n, k; cin >> n >> k;
+    f0(i, n) cin >> a[i];
+    sort(a, a+n);
+    ll ans = 0;
+    for(int i = 0; i<n-1; i++){
+        //a[i]=>a[j]>k-a[i]
+        int l = first_pos_greater(a, i+1, n-1, k-a[i]);
+        if(l != -1)
+            ans += n-1 - l + 1;
+    }
+    cout << ans << endl;
+}
+//----------------------------------------------------------------
+/** Tìm tích của số lớn nhất của mảng a và nhỏ nhất của mảng b 
+ * K dùng cách sort để lấy max và min của mảng 
+2 
+3 4
+1 2 3 
+-2 3 4 
+=> -6
+1 1 
+1000000000
+1000000000
+=> 10^18
+*/
+//int a[1000001];
+int b[1000001];
+void run9(){
+    int n, m; cin >> n >> m;
+    f0(i, n) cin >> a[i];
+    f0(i, n) cin >> b[i];
+    // HÀM TÌM PHẦN TỬ NHỎ NHẤT TRONG ĐOẠN/MẢNG O(n)
+    // max_element trả về iterator nên dùng * để lấy giá trị 
+    int m1 = *max_element(a, a+n);
+    int m2 = *min_element(b, b+m);
+    cout << 1ll*m1*m2 << endl;//tràn số 
+}
+//-----------------------------------------------------------
+/**Hợp nhất 2 mảng 
+1
+3 4 
+1 2 3 
+1 5 6 2 
+=> 1 1 2 2 3 5 6
+*/
+void run10(){
+    int n, m; cin >> n >> m;
+    int arr[n+m];
+    for(int i=0; i<n+m; i++) cin >> arr[i];
+    sort(arr, arr + n + m);
+    for(int i=0; i<n+m; i++)
+        cout << arr[i] << " ";
+    el;
+}
+//---------------------------------------------------------
+/**Điền số còn thiếu 
+ * Cho mảng a, gọi L và R là min và max. Tìm số phần tử cần thêm vào mảng
+ * để mảng có đầy đủ các số trong khoảng [L,R].
+ * VD {5,7,9,3,6,2} thiếu 4,8
+Mảng đánh dấu 
+2
+5
+4 5 3 8 6 
+3
+2 1 3
+*/
+void run11(){
+    int n; cin >> n;
+    int l = INT_MAX;
+    int r = INT_MIN;
+    bool a[1000001] = {0};
+    for(int i = 0; i < n; i++){
+        int x; cin >> x;
+        l = min(l, x);
+        r = max(r, x);
+        a[x] = 1;
+    }
+    int cnt = 0;
+    for (int i=l; i<=r; i++){
+        if(a[i] == 0)
+            ++cnt;
+    }
+    cout << cnt << endl;
+}
+//----------------------------------------------------------
+/**Sắp xếp theo giá trị tuyệt đối của |X-a[i]|
+ * cùng giá trị tuyệt đối thì ưu tiên thứ tự xh trong mảng 
+2
+5 7 
+10 5 3 9 2 
+5 6 
+1 2 3 4 5 
+=>
+5 9 10 3 2 
+5 4 3 2 1
+*/
+int n, x;
+bool cmpAbs(int a, int b){
+    return abs(x-a) < abs(x-b);
+}
+void run12(){
+    cin >> n >> x;
+    f0(i, n) cin >> a[i];
+    stable_sort(a, a+n, cmpAbs);//giữ nguyên thứ tự xh
+    f0(i, n) cout << a[i] << " ";
+    el;
+}
+
+//C2: lưu ptu trong mảng kèm theo chỉ số xh 
+bool cmpAbsXh(pair<int, int> a, pair<int, int> b){
+    int abs1 = abs(a.first - x);
+    int abs2 = abs(b.first - x);
+    if(abs1 != abs2)
+        return abs1 < abs2;
+    return a.second < b.second;
+}
+void run13(){
+    cin >> n >> x;
+    pair<int, int> v[n];
+    f0(i,n){
+        cin >> v[i].first;
+        v[i].second = i;
+    }
+    sort(v, v+n, cmpAbsXh);
+    f0(i,n) cout << v[i].first << " ";el;
+}
+//----------------------------------------------------------------
+/** Hợp và giao của 2 mảng 
+ * Giống merge sort, xh ở 2 mảng thì cho vào giao, 
+ * <, giống xh thì cho vào hợp 
+3 5 
+1 2 3 
+1 2 3 5 9 
+=>
+1 2 3 5 9 
+1 2 3
+*/
+void run(){
+    int n, m; cin >> n >> m;
+    int a[n], b[m];
+    f0(i,n) cin >> a[i]; 
+    f0(i,m) cin >> b[i];
+    int i = 0, j = 0;
+    vector<int> giao, hop;
+    while(i < n && j < m){
+        if(a[i] < b[j]){
+            hop.push_back(a[i]);
+            ++i;
+        }else if(a[i] > b[j]){
+            hop.push_back(b[j]);
+            ++j;
+        }else{
+            hop.push_back(a[i]);
+            giao.push_back(a[i]);
+            ++i; ++j;
+        }
+    }
+    while(i < n)
+        hop.push_back(a[i++]);
+    
+    while(j < m)
+        hop.push_back(b[j++]);
+    
+    for(int x : hop) cout << x << " ";el;
+    for(int x : giao) cout << x << " ";el;
 }
 int main() {
     ios_base::sync_with_stdio(0);
